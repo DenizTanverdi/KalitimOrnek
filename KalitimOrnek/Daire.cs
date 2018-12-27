008083d266d348885d59af3079d63281b42f4ce3
semvs <semvs@ayvansaray.edu.tr>
2018-12-27 13:48:30 +0300

    Daire Eklendi

---
+++ KalitimOrnek/Daire.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalitimOrnek
+{
+    class Daire : Sekil
+    {
+        double yariCap;
+        double pi = Math.PI;
+        public double YariCap { get { return yariCap; } set { yariCap = value; } }
+        public Daire(double y)
+        {
+            yariCap = y;
+            this.alan = pi * Math.Pow(yariCap, 2);
+            this.cevre = 2 * pi * yariCap;
+        }
+    }
+}
--- KalitimOrnek/Program.cs
+++ KalitimOrnek/Program.cs
@@ -13,12 +13,15 @@ namespace KalitimOrnek
             kare k = new kare(5);
             dikDörtgen dk = new dikDörtgen(5,3);
             KarePrizma kp = new KarePrizma(5,4);
+            Daire d = new Daire(3);
             Console.WriteLine($"Karenin Alan :{k.alan}");
             Console.WriteLine($"Karenin Cevresi:{ k.cevre}");
             Console.WriteLine($"Dikdörtgenin Alanı :{dk.alan}");
             Console.WriteLine($"Dikdörtgenin Cevresi:{ dk.cevre}");
             Console.WriteLine($"Kare Prizmanın Alanı :{kp.alan}");
             Console.WriteLine($"Kare Prizmanın Hacmi:{ kp.hacim}");
+            Console.WriteLine($"Dairenın Alanı :{d.alan}");
+            Console.WriteLine($"DDairenın Cevresi:{ d.cevre}");
 
             Console.ReadLine();
                 
